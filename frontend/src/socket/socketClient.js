0f3c488ff5c60409fe8614f2877d50bddbe64e9a
mariajuliapessoa <mjpc@cesar.school>
2026-06-08 16:52:38 -0300

    Configura URL do backend em produção

--- frontend/src/socket/socketClient.js
+++ frontend/src/socket/socketClient.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
+const socket = io('https://jogo-da-memoria-k2su.onrender.com', {
   autoConnect: false,
 });
 
